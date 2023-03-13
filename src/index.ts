3d57eb2b4940f357c382a6fb7430a9f854e45a16
Kyosuke <kyosuke@Kyosukes-MacBook-Pro-lacicu.local>
2023-03-13 09:22:19 +0900

    es-lint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-// import MyCounter from '../example/src/components/App'
-// import { Sort } from './components/table/Table'
-// import { FilterType } from './components/filter/Enums'
-// import { FilterTable } from './components/filtertable/FilterTable'
-//
-// // sample component
-// export {MyCounter}
-//
-// // components for filter table
-// export { Sort, FilterType, FilterTable }
+import MyCounter from './components/App'
+import { Sort } from './components/table/Table'
+import { FilterType } from './components/filter/Enums'
+import { FilterTable } from './components/filtertable/FilterTable'
+
+// sample component
+export { MyCounter }
+
+// components for filter table
+export { Sort, FilterType, FilterTable }
