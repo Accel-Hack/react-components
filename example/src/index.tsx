a68dbcaeee6c64f75edf4a2cf21a8fa41be3cb3d
Kyosuke <kyosuke@Kyosukes-MacBook-Pro-lacicu.local>
2023-03-15 09:36:53 +0900

    Update readme & live demo

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,51 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ARFilterTable, ARFilterType, ARSort, ARTableFunctions } from 'ah-react-components'
+import { ARFilterTable, ARTable, FilterType } from 'ah-react-components'
 
-const filters = [
-  {
-    type: ARFilterType.TEXT,
-    name: '検索ワード',
-    field: 'word',
-  },
-  {
-    type: ARFilterType.TOGGLE,
-    name: '大文字・小文字',
-    field: 'font',
-    options: [
-      { caption: 'ABC', value: 'upper' },
-      { caption: 'abc', value: 'lower' },
-    ],
-  },
-]
-
-const columns = [
-  { field: 'id', children: 'ID', sortable: true },
-  { field: 'name', children: '名前', sortable: true },
-  { field: 'desc', children: '説明' },
-]
-
-const options = {
-  selectable: { enabled: true, identifier: 'id' },
-}
-
-const getRows = async (limit: number, offset: number, sort: ARSort[], options?: any[]) => {
-  console.log(
-    `getRows(limit: ${limit}, offset: ${offset}, sort: ${JSON.stringify(sort)}, options: ${JSON.stringify(options)})`,
-  )
-  return {
-    total: 10,
-    rows: [
-      { id: 1, name: 'aa', desc: 'desc1' },
-      { id: 2, name: 'aa', desc: 'desc1' },
-      { id: 3, name: 'aa', desc: 'desc1' },
-    ],
-  }
-}
-
-const func: ARTableFunctions = {
+const table = new ARTable.Class({
+  options: { selectable: { enabled: true, identifier: 'id' } },
+  columns: [
+    { field: 'id', children: 'ID', sortable: true },
+    { field: 'name', children: '名前', sortable: true },
+    { field: 'desc', children: '説明' },
+  ],
   delegate: {
-    getRows,
+    getRows: async (limit, offset, sort, options?) => {
+      console.log(`getRows(${limit}, ${offset}, ${JSON.stringify(sort)}, ${JSON.stringify(options)})`)
+      return {
+        total: 10,
+        rows: [
+          { id: 1, name: 'aa', desc: 'desc1' },
+          { id: 2, name: 'aa', desc: 'desc1' },
+          { id: 3, name: 'aa', desc: 'desc1' },
+        ],
+      }
+    },
     onRowClick: (row: any) => {
       console.log('onRowClick', row)
     },
@@ -53,24 +28,86 @@ const func: ARTableFunctions = {
       console.log('onDataLoaded')
     },
   },
-}
+})
+
+const filterTable = new ARFilterTable.Class({
+  filters: [
+    { type: FilterType.TEXT, name: '検索ワード', field: 'word' },
+    {
+      type: FilterType.TOGGLE,
+      name: '大文字・小文字',
+      field: 'font',
+      options: [
+        { caption: 'ABC', value: 'upper' },
+        { caption: 'abc', value: 'lower' },
+      ],
+    },
+  ],
+  options: { selectable: { enabled: true, identifier: 'id' } },
+  columns: [
+    { field: 'id', children: 'ID', sortable: true },
+    { field: 'name', children: '名前', sortable: true },
+    { field: 'desc', children: '説明' },
+  ],
+  delegate: {
+    getRows: async (limit, offset, sort, options?) => {
+      console.log(`getRows(${limit}, ${offset}, ${JSON.stringify(sort)}, ${JSON.stringify(options)})`)
+      return {
+        total: 10,
+        rows: [
+          { id: 1, name: 'aa', desc: 'desc1' },
+          { id: 2, name: 'aa', desc: 'desc1' },
+          { id: 3, name: 'aa', desc: 'desc1' },
+        ],
+      }
+    },
+    onRowClick: (row: any) => {
+      console.log('onRowClick', row)
+    },
+    onDataLoaded: () => {
+      console.log('onDataLoaded')
+    },
+  },
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <div>
-      <h2>Sample Table With Filter</h2>
+      <h2>Sample Table</h2>
       <div>
-        <ARFilterTable filters={filters} columns={columns} func={func} options={options} />
+        <ARTable.Component table={table} />
         <button
           onClick={() => {
-            console.log('get Rows', func.dispatch?.getRows())
+            console.log('get Rows', table.getRows())
           }}
         >
           get Rows
         </button>
         <button
           onClick={() => {
-            console.log('get selected rows', func.dispatch?.getSelectedRows?.())
+            console.log('get selected rows', table.getSelectedRows())
+          }}
+        >
+          get selected rows
+        </button>
+      </div>
+    </div>
+    <hr></hr>
+    <div>
+      <h2>Sample Table with Filter</h2>
+      <div>
+        <ARFilterTable.Component table={filterTable} />
+        <button
+          onClick={() => {
+            console.log('get Rows', filterTable.getRows())
+          }}
+        >
+          get Rows
+        </button>
+        <button
+          onClick={() => {
+            console.log('get selected rows', filterTable.getSelectedRows?.())
           }}
         >
           get selected rows
